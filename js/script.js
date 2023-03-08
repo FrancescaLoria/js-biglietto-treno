947d1b15a33442a5a01c2e1c6a581922931b2a5f
FrancescaLoria <francesca.loria90@hotmail.it>
2023-03-08 15:07:30 +0100

    questions and calculation of kilometers

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca
+
+// CHIEDIAMO ALL'UTENTE IL NUMERO DI CHILOMETRI CHE VUOLE PERCORRERE.
+const userKilometres = prompt("Quanti chilometri vuoi percorrere?");
+console.log(userKilometres);
+
+// CHIEDIAMO ALL'UTENTE QUANTI ANNI HA.
+const userAge = prompt("Quanti anni hai?");
+console.log(userAge);
+
+// SULLE BASI DELLA PRIMA DOMANDA CALCOLIAMO IL PREZZO INTERO DEL BIGLIETTO
+const priceForKm = 0.21;
+const totalPrice = userKilometres * priceForKm;
+console.log(totalPrice);
+
+// SULLA BASE DELL'ETA' DEL NOSTRO UTENTE CALCOLIAMO LO SCONTO
